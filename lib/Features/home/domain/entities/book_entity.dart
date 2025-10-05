a6b66d1d9fbdce02bc95aae2cb7dd9904962cf73
Salah Saleh <salahsaleh1015@gmail.com>
2025-10-05 14:10:22 +0300

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String title;
+  final String authorName;
+  final String image;
+  final num price;
+  final num rate;
+
+  BookEntity(this.title, this.authorName, this.image, this.price, this.rate);
+}
