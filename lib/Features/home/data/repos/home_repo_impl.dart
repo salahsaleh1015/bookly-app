de613ec630099d302748043cbd3fad4cae1c750f
Salah Saleh <salahsaleh1015@gmail.com>
2025-10-09 15:16:50 +0300

    create HomeRepoImpl

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,22 @@
+
+
+
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+
+}
