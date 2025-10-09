c141fbd1a045df2b9ecf049a4934cfdb21b32671
Salah Saleh <salahsaleh1015@gmail.com>
2025-10-09 14:07:45 +0300

    create home local data source

---
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,27 @@
+
+
+
+
+
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+ List<BookEntity> fetchFeaturedBooks();
+ List<BookEntity> fetchNewestBooks();
+}
+
+
+class HomeLocalDataSourceImpl implements HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+
+}
